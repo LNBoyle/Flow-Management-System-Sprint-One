b7a496ece4b5625661604fffeddd6592500ac499
Sebastian <Sebastian@134.36.36.100>
2019-01-21 00:47:29 +0000

    Inital Commit which contains a basic hard coded login page (no database connection). 2 jsp files; staffCheckLoginCredentials.jsp which contains the interface code between the login form and the LoginCheckClass.java which contains the actual checking code (hard coded but will contain database connection later), index.jsp; contains the front end for the login form. LoginCheckClass.java is the java class which deals with confirming if the login credentials are correct. css, images and scrips folders added to contain the correct files within them. staffLogin.css contains the styling for the login page. staffLogin.js contains the javascript for the login page. jquery-1.11.1.js contains the code to be able to use jquery within the whole site. apart from the images in the image folder the rest of the files are pre generated by netbeans but are essential.

---
+++ src/java/ExamWebbApp/LoginCheckClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ExamWebbApp;
+
+public class LoginCheckClass {
+    
+    public static String makeItLower(String input){
+        return input.toLowerCase();
+    }
+    
+    public static boolean checkStaffLogin(String userEmail, String userPassword){
+        String email = "test.user@dundee.ac.uk";
+        String password = "Test123";
+        
+        if(email.equals(userEmail) && password.equals(userPassword)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
