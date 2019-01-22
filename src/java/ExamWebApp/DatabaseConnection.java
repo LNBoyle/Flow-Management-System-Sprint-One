25c8d7a401c8152bceb8db14ce7c39d1948f292c
matthewdaldry <matthewdaldry@labpc253.computing.dundee.ac.uk>
2019-01-22 17:56:29 +0000

    Added webpage for adding responses to the database. DatabaseConnection java file now has functions that support this page.

--- src/java/ExamWebApp/DatabaseConnection.java
+++ src/java/ExamWebApp/DatabaseConnection.java
@@ -7,7 +7,7 @@ package ExamWebApp;
 import java.sql.*;  
 /**
  *
- * @author Sebastian
+ * 
  */
 public class DatabaseConnection {
     
@@ -49,4 +49,77 @@ public class DatabaseConnection {
         }
         return false;
     } 
+    
+    //Function that returns the module code for a given exam
+    public String getExamModule(int examID)
+    {
+        //Try block to add the repsonse to the comment
+        try
+        {
+            stmt = conn.createStatement();
+            reslt = stmt.executeQuery("SELECT ModuleCode FROM exam WHERE ExamID = " + examID + ";");
+            
+            //return string from query
+            if (reslt.next())
+            {
+                return reslt.getString(1);   
+            }            
+        }
+        //Catch block for errors with SQL
+        catch(SQLException e)
+        {
+            System.out.println("Error: " + e);
+        }
+        return null;
+    }
+    
+    //Function that returns the comment for a given exam and staff role
+    public String getExamComment(int commentID)
+    {
+        //Try block to add the repsonse to the comment
+        try
+        {
+            stmt = conn.createStatement();
+            reslt = stmt.executeQuery("SELECT Comment FROM comment WHERE CommentID = " + commentID + ";");
+            
+            //return string from query
+            if (reslt.next())
+            {
+                return reslt.getString(1);   
+            }            
+        }
+        //Catch block for errors with SQL
+        catch(SQLException e)
+        {
+            System.out.println("Error: " + e);
+        }
+        return null;
+    }
+    
+    //Function that adds a response to a comment in the database
+    public boolean setCommentResponse(int commentID, String newResponse)
+    {
+        //Try block to add the repsonse to the comment
+        try
+        {
+            stmt = conn.createStatement();
+            int success = stmt.executeUpdate("UPDATE comment SET Response = '" + newResponse + "' WHERE CommentID = " + commentID + ";");
+            
+            //return true if success, false otherwise
+            if (success != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //Catch block for errors with SQL
+        catch(SQLException e)
+        {
+            System.out.println("Error: " + e);
+        }
+        return false;
+    }
 }
---
+++ web/commentRespond.jsp
@@ -0,0 +1,42 @@
+<%@page language="java" import="ExamWebApp.*" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%DatabaseConnection db = new DatabaseConnection();%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>EFMS</title>
+    </head>
+    <body>
+        <%-- Fetch and print exam name and the associated comment --%>
+        <h1>
+            Exam: <% out.print(db.getExamModule(1)); %> <br>
+            Comment: <% out.print(db.getExamComment(1)); %> <br>
+            <%-- Form for user to input response to comment and button to sign their response --%>
+            <form method = "POST">
+                Your Response: <input type = "textarea" name = "response" size = 150px required> <br>
+                <button type = "submit" name = "responseSubmit"> Sign & Submit</button>
+            </form>
+        </h1>
+
+        <%-- Check response on submit and update the database. Tell user if successful. --%>
+        <%
+            if ((request.getParameter("responseSubmit") != null))
+            {
+                if ((db.setCommentResponse(1, request.getParameter("response"))) == true)
+                {
+                    out.println("Success!");
+                    out.println(
+                            "<form method = 'POST' action = 'index.jsp'>"
+                            + "<button type = 'submit' name = 'ReturnFromSubmit'>Return</button>"
+                            + "</form>"
+                    );
+                }
+                else
+                {
+                    out.println("Failure!");
+                }
+            }
+        %>
+    </body>
+</html>
