649ebc2a3278bcf37152f7bc8168967e99462ab6
Sebastian6656 <s.salek@dundee.ac.uk>
2019-01-24 20:14:16 +0000

    Added functionality so that each type of user can see their assigned exams and print them in a table.

--- src/java/ExamWebApp/DatabaseConnection.java
+++ src/java/ExamWebApp/DatabaseConnection.java
@@ -603,6 +603,68 @@ public class DatabaseConnection {
 	return null;
 }
     
+    
+    public String[][] getExamLists()
+{    ResultSet rs;
+	try
+	{
+            stmt = conn.createStatement();
+            
+            switch (LoginCheckClass.userRole) {
+                case "Internal Moderator":
+                    {
+                        rs = stmt.executeQuery("SELECT ExamID,Title,ModuleCode,ModuleCoordinator FROM exam WHERE InternalModerator = '" + LoginCheckClass.userID + "' ;");
+                        break;
+                    }
+                case "External Examiner":
+                    {
+                        rs = stmt.executeQuery("SELECT ExamID,Title,ModuleCode,ModuleCoordinator FROM exam WHERE ExternalExaminer = '" + LoginCheckClass.userID + "' ;");
+                        break;
+                    }
+                case "Exam Vetting Comittee":
+                    {
+                        rs = stmt.executeQuery("SELECT ExamID,Title,ModuleCode,ModuleCoordinator FROM exam WHERE ExamVettingComittee = '" + LoginCheckClass.userID + "' ;");
+                        break;
+                    }
+                default:
+                    return null;
+            }
+            
+            
+            //ResultSet rs = stmt.executeQuery("SELECT ExamID,Title,ModuleCode FROM exam WHERE ModuleCoordinator = '" + ModuleCoordinator + "' ;");
+
+            int row = 0;
+            if (rs.last())
+            {
+                    row = rs.getRow();
+                    rs.beforeFirst();
+            }
+            CompletedRowss = row;
+            String[][] staffExams = new String[row][4];
+            int j = 0;
+            while(rs.next())
+            {
+                    staffExams[j][0] = Integer.toString(rs.getInt("ExamID"));
+                    staffExams[j][1] = rs.getString("Title");
+                    staffExams[j][2] = rs.getString("ModuleCode");
+                    staffExams[j][3] = rs.getString("ModuleCoordinator");
+                    j++;
+            }
+            if(staffExams != null)
+            {
+                    return staffExams;
+            } else {
+                    System.out.println("The return is Null");
+            }
+	}
+	catch(SQLException e)
+	{
+		System.out.println("Error: " + e);
+	}
+	return null;
+}
+    
+    
 
 }
 
--- web/ExternalExaminerDashboard.jsp
+++ web/ExternalExaminerDashboard.jsp
@@ -4,6 +4,7 @@
     Author     : Sebastian
 --%>
 
+<%@page import="ExamWebApp.*"%>
 <%@page import="ExamWebApp.DeadLine"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -13,17 +14,49 @@
         <title>JSP Page</title>
     </head>
     <body>
-         <div class="topnav">
-            <a class="active" href="#">Home</a>
-            <a href="#Lock">Lock</a>
-            <a href="#Back">Back</a>
-            <h1 style="font-size:17px;float:right;padding:0px 10px;">Welcome...</h1>
-        </div>
+         <div id="header">
+            <nav class="navbar navbar-light">
+                    <span class="navbar-brand">Welcome..</span>
+            </nav>
+
+            <a id="home" href="SchoolOfficeDashboard.jsp" class="btn btn-blue btn-lg toggle"><i class="fa fa-home"></i></a>
+            <a id="logout" href="index.jsp" class="btn btn-blue btn-lg toggle"><i class="fa fa-lock"></i></a>
+
+	</div>
+        
+        <br>
         
         <%
             DeadLine deadline = new DeadLine();
             out.print("Your Deadline to complete your exams is: " + deadline.deadline());
             
             %>
+            <br>
+             <%
+            DatabaseConnection db = new DatabaseConnection();
+            String[][] exam = db.getExamLists();
+            
+            
+            %>
+            <table>
+        <tr>
+            <th class="headerTable">Exam ID</th>
+            <th class="headerTable">Exam Title</th>
+            <th class="headerTable">Module Code</th>
+            <th class="headerTable">Module Coordinator</th>
+        </tr>
+        <%
+        for(int i=0;i<exam.length;i++){
+        %>
+        <tr class='clickable-row' data-toggle="modal">
+                <td><%out.print(exam[i][0]);%></td>
+                <td><%out.print(exam[i][1]);%></td>
+                <td><%out.print(exam[i][2]);%></td>
+                <td><%out.print(exam[i][3]);%></td>
+            </tr>
+        <%
+        }
+        %>
+    </table>
     </body>
 </html>
