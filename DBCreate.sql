2ba5d27ae26ae917d9019328059fcd1f37a8ee23
cas1332 <39855141+cas1332@users.noreply.github.com>
2019-01-24 16:43:50 +0000

    Update DBCreate.sql

--- DBCreate.sql
+++ DBCreate.sql
@@ -189,19 +189,23 @@ UNLOCK TABLES;
 # ------------------------------------------------------------
 
 DROP VIEW IF EXISTS `assignedroles`;
-CREATE VIEW `assignedroles` AS
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `18agileteam8`@`%` 
+    SQL SECURITY DEFINER
+VIEW `assignedroles` AS
     SELECT 
-        a.`ExamID` AS `ExamID`,
-        b.`UserID` AS `UserID`,
-        b.`FirstName` AS `FirstName`,
-        b.`Surname` AS `Surname`,
-        b.`Role` AS `Role`
+        `a`.`ExamID` AS `ExamID`,
+        `b`.`UserID` AS `UserID`,
+        `b`.`FirstName` AS `FirstName`,
+        `b`.`Surname` AS `Surname`,
+        `b`.`Role` AS `Role`
     FROM
-        (`exam` a
-        JOIN `user` b)
+        (`exam` `a`
+        JOIN `user` `b`)
     WHERE
-        ((a.`AuthorID` = b.`UserID`)
-            OR (a.`AssignedTo` = b.`UserID`))
+        ((`a`.`AuthorID` = `b`.`UserID`)
+            OR (`a`.`AssignedTo` = `b`.`UserID`))
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
