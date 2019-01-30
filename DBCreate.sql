7476cae4b7d5f50601651f8624097ce36339d912
LumpyLobster <mfdaldry@yahoo.com>
2019-01-30 12:55:47 +0000

    Updated Assignedexams table
    Nullable values for internal mod, external examiner and exam vet. All exams are also in assigned exams.

--- DBCreate.sql
+++ DBCreate.sql
@@ -104,9 +104,9 @@ CREATE TABLE `ASSIGNEDEXAMS` (
   `ExamPeriod` varchar(12) NOT NULL,
   `ExamLevel` varchar(20) NOT NULL,
   `ExamSetter` int(10) NOT NULL,
-  `InternalModerator` int(10) NOT NULL,
-  `ExternalExaminer` int(10) NOT NULL,
-  `ExamVettingComittee` int(10) NOT NULL,
+  `InternalModerator` int(10),
+  `ExternalExaminer` int(10),
+  `ExamVettingComittee` int(10),
   PRIMARY KEY (`AssignedExamID`),
   KEY `pk_AssignedExamID` (`AssignedExamID`),
   KEY `fk_EXAMSET` (`ExamSetter`),
@@ -128,6 +128,8 @@ VALUES
   (1,'AC310001','Main','Undergraduate',00010007,00010001,00010003,00010004),
   (2,'AC330001','Main','Undergraduate',00010007,00010001,00010003,00010004),
   (3,'AC320001','Main','Postgraduate',00010007,00010001,00010003,00010004);
+  (4,'AC350001','Resit','Postgraduate',00010007,00010001,00010003,00010004);
+  (5,'AC380001','Main','Undergraduate',00010007,00010001,00010003,00010004);
 
 /*!40000 ALTER TABLE `ASSIGNEDEXAMS` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -161,7 +163,7 @@ VALUES
 
   (00000001,'Agile Software Engineering','Science and Engineering','Iain Murray','AC310001','Online','Main','Undergraduate','2','2019','New',NULL,NULL),
   (00000002,'AI and Algorithims','Science and Engineering','Iain Murray','AC330001','Online','Main','Undergraduate','1','2020','In Progress',NULL,NULL),
-  (00000003,'Multi Paradigm','Science and Engineering','Iain Murray','AC320001','Online','Main','Postgraduate','2','2019','In Progress',NULL,NULL),
+  (00000003,'Multi Paradigm','Science and Engineering','Iain Murray','AC320001','Online','Main','Postgraduate','2','2019','Completed',NULL,NULL),
   (00000004,'Web Authoring','Science and Engineering','Iain Murray','AC350001','Paper','Resit','Postgraduate','1','2020','Completed',NULL,NULL),
   (00000005,'Data Structures','Science and Engineering','Iain Murray','AC380001','Paper','Main','Undergraduate','2','2019','Completed',NULL,NULL);
 
